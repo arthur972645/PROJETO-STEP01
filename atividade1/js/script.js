fc595e06cb758ff49df629862149dc732d361a72
arthur972645 <138785050+arthur972645@users.noreply.github.com>
2024-03-08 22:03:12 -0300

    logica feita

--- atividade1/js/script.js
+++ atividade1/js/script.js
@@ -3,33 +3,68 @@ const categoria = document.querySelector("#opcaoCategoria")
 const turno = document.querySelector("#opcaoTurno")
 const horasTrabalhadas = document.querySelector("#HorasTrabalhadas") 
 const btn = document.querySelector("#btn")
-const horaValor = document.querySelector("#horaValor")
+const mostraCodigo = document.querySelector("#mostraCodigo")
+const valoPorHora = document.querySelector("#valorHora")
+const mostrarHorasTrabalhadas = document.querySelector("#mostrarHorasTrabalhadas")
+const valorMensalSemAuxilio = document.querySelector("#valorMensalSemAuxilio")
+const auxilioAlimentacao = document.querySelector("#auxilioAlimentacao")
+const salarioFinal = document.querySelector("#salarioFinal")
+
 btn.addEventListener('click',()=>{
-    const mostrarId = id.value
+    const mostraId = id.value
     const mostrarCategoria = categoria.value
     const mostrarTurno = turno.value
     const mostrarHoras = horasTrabalhadas.value
     
-    valorHoraTrabahada(mostrarCategoria, mostrarTurno,mostrarHoras)
+    valorHoraTrabahada(mostrarCategoria, mostrarTurno,mostrarHoras,mostraId)
 
     
 })
 
-function valorHoraTrabahada (mostrarCategoria, mostrarTurno, mostrarHoras){
-    let resultadoHoras
+function valorHoraTrabahada (mostrarCategoria, mostrarTurno, mostrarHoras,mostraId){
+    // const horasTrabalhadas = document.querySelector("#HorasTrabalhadas") 
     
+    // const quantidadDeHorasTrabalhadas = horasTrabalhadas.value
+   
+    
+    let valorHora
+    let valorMensal
+    let valorAuxilio
+    let valorTotal
+    
+    debugger;
     if(mostrarCategoria === 'Gerente' && mostrarTurno === 'Noturno'){
-          resultadoHoras = (0.18 * 450)*mostrarHoras
-          horaValor.innerHTML = (resultadoHoras)
+          valorHora = 0.18 * 450
+          valorMensal = valorHora * mostrarHoras
+          if(valorMensal <= 300){
+            valorAuxilio = 0.20 * valorMensal
+            valorTotal = valorMensal + valorAuxilio
+
+          }
+          mostraCodigo.innerHTML = (`Codigo do trabalhador: ${mostraId}`)
+          mostrarHorasTrabalhadas.innerHTML = (`Foram trabalhadas: ${mostrarHoras} horas`)
+          valoPorHora.innerHTML = (`Valor por hora: $${valorHora}`)
+          valorMensalSemAuxilio.innerHTML = (`Valor por horas trabalhadas: $${valorMensal}`)
+          auxilioAlimentacao.innerHTML = (`O valor do auxio: $${valorAuxilio}`)
+          salarioFinal.innerHTML = (`O valor total será de: $${valorTotal}`)
     }
     else if(mostrarCategoria === 'Gerente' && mostrarTurno === 'Matutino' || mostrarTurno === 'Vespertino'){
-        resultadoHoras === (0.15 * 450)*mostrarHoras
+        valorHora = 0.15 * 450
+        valorMensal = valorHora * mostrarHoras
+        valoPorHora.innerHTML = (`Valor por hora: $${valorHora}`)
+        valorMensalSemAuxilio.innerHTML = (`Valor por horas trabalhadas: $${valorMensal}`)
     }
     else if(mostrarCategoria == 'Operario' && mostrarTurno === 'Noturno'){
-        resultadoHoras === (0.13 * 450)*mostrarHoras
+        valorHora = 0.13 * 450
+        valorMensal = valorHora * mostrarHoras
+        valoPorHora.innerHTML = (`Valor por hora: $${valorHora}`)
+        valorMensalSemAuxilio.innerHTML = (`Valor por horas trabalhadas: $${valorMensal}`)
     }
     else if(mostrarCategoria === 'Operario' && mostrarTurno === 'Matutino' || mostrarTurno === 'Vespertino'){
-        resultadoHoras === (0.13 * 450)*mostrarHoras
+        valorHora = 0.10 * 450
+          valorMensal = valorHora * mostrarHoras
+          valoPorHora.innerHTML = (`Valor por hora: $${valorHora}`)
+          valorMensalSemAuxilio.innerHTML = (`Valor por horas trabalhadas: $${valorMensal}`)
     }
 
 }
